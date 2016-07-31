8777b478f0e1a8c8831f837008ba1f9b3539695a
Bryce DesBrisay <brycedesbrisay@Bryces-MacBook-Air.local>
2016-07-31 15:44:13 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const potato = 'potato';
+let varr = 'hello';
+
+console.log(varr);
+
+function sum (num1, num2) {
+    return num1 + num2;
+}
+
+function sayHello (firstName, lastName) {
+    return `Hello ${firstName} ${lastName}, welcome to JS`;
+};
+
+console.log(sayHello('bryce', 'desbrisay'));
